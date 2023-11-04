3e0dad62c34fac4b766664a3a6948e4d410743a9
scal3 <cool.goga501@gmail.com>
2023-11-04 14:16:41 +0100

    feat/create model validation for UserController and FilmController

--- src/main/java/com/example/filmorate/controller/FilmController.java
+++ src/main/java/com/example/filmorate/controller/FilmController.java
@@ -1,8 +1,11 @@
 package com.example.filmorate.controller;
 
+import com.example.filmorate.exception.ValidationException;
 import com.example.filmorate.model.Film;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+import java.time.Month;
 import java.util.List;
 
 @RequestMapping("/film")
@@ -15,11 +18,41 @@ public class FilmController {
 
     @PostMapping
     public Film createFilm(@RequestBody Film film) {
+        validateFilmModel(film);
+
         return film;
     }
 
     @PutMapping
     public Film updateFilm(@RequestBody Film film) {
+        validateFilmModel(film);
+
         return film;
+    }
+
+    private void validateFilmModel(Film film) throws ValidationException {
+        if (film.getDuration() == null
+                || film.getName() == null
+                || film.getDescription() == null
+                || film.getReleaseDate() == null
+        ) {
+            throw new ValidationException("Some field is null");
+        }
+
+        if (film.getName().isEmpty()) {
+            throw new ValidationException("Film name is empty");
+        }
+
+        if (film.getDescription().length() > 200) {
+            throw new ValidationException("Film description is too long");
+        }
+
+        if (film.getReleaseDate().isBefore(LocalDate.of(1985, Month.DECEMBER, 28))) {
+            throw new ValidationException("Film release date is too old");
+        }
+
+        if (film.getDuration().isNegative()) {
+            throw new ValidationException("Film duration is negative");
+        }
     }
 }
--- src/main/java/com/example/filmorate/controller/UserController.java
+++ src/main/java/com/example/filmorate/controller/UserController.java
@@ -1,8 +1,10 @@
 package com.example.filmorate.controller;
 
+import com.example.filmorate.exception.ValidationException;
 import com.example.filmorate.model.User;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RequestMapping("/user")
@@ -15,11 +17,45 @@ public class UserController {
 
     @PostMapping
     public User createUser(@RequestBody User user) {
+        validateUserModel(user);
+
+        if (user.getName().trim().isEmpty()) {
+            user.setName(user.getLogin());
+        }
+
         return user;
     }
 
     @PutMapping
     public User updateUser(@RequestBody User user) {
+        validateUserModel(user);
+
+        if (user.getName().trim().isEmpty()) {
+            user.setName(user.getLogin());
+        }
+
         return user;
+    }
+
+    private void validateUserModel(User user) throws ValidationException {
+        if (user.getName() == null
+                || user.getEmail() == null
+                || user.getBirthday() == null
+                || user.getLogin() == null
+        ) {
+            throw new ValidationException("Some field is null");
+        }
+
+        if (user.getEmail().isEmpty() || !user.getEmail().contains("@")) {
+            throw new ValidationException("User email is incorrect");
+        }
+
+        if (user.getLogin().trim().isEmpty()) {
+            throw new ValidationException("User login is incorrect");
+        }
+
+        if (user.getBirthday().isAfter(LocalDate.now())) {
+            throw new ValidationException("User birthday is incorrect");
+        }
     }
 }
