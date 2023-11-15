dc3dcd35b12958fb0a10a206761430206ae6507e
scal3 <cool.goga501@gmail.com>
2023-11-15 15:23:22 +0100

    fix/codestyle

--- src/main/java/ru/yandex/practicum/filmorate/film/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/film/storage/FilmStorage.java
@@ -6,9 +6,14 @@ import java.util.List;
 
 public interface FilmStorage {
     Film getOneById(int id);
+
     Film getOneByName(String name);
+
     List<Film> getAll();
+
     Film create(Film film);
+
     Film update(Film film);
+
     void delete(int id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/user/storage/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/user/storage/UserStorage.java
@@ -6,9 +6,14 @@ import java.util.List;
 
 public interface UserStorage {
     User getOneById(int id);
+
     User getOneByName(String name);
+
     List<User> getAll();
+
     User create(User user);
+
     User update(User user);
+
     void delete(int id);
 }
